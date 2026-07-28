26037c69d9e48d17ea84fa98aff89ae08d717c04
Priya <77498034+PriyaA431@users.noreply.github.com>
2026-07-28 23:25:19 +0530

    Time: 3 ms (90.16%), Space: 19.5 MB (40.97%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -2,7 +2,6 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         m, n = len(s), len(p)
 
-        # dp[i][j] = True if s[:i] matches p[:j]
         dp = [[False] * (n + 1) for _ in range(m + 1)]
         dp[0][0] = True
 
