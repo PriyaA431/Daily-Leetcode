f734cca00d6881b7dd96f5be401501c27bf4008b
Priya <77498034+PriyaA431@users.noreply.github.com>
2026-08-03 23:32:24 +0530

    Time: 11 ms (13.85%), Space: 19.4 MB (19.2%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -15,21 +15,3 @@ class Solution:
                 
         return total
 
-#------------------------
-        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        
-        total = 0
-        prev_value = 0
-        
-        # Traverse right to left
-        for char in reversed(s):
-            curr_value = roman_map[char]
-            
-            # If value dropped, subtract it (e.g., IV where V=5, I=1)
-            if curr_value < prev_value:
-                total -= curr_value
-            else:
-                total += curr_value
-                prev_value = curr_value
-                
-        return total
